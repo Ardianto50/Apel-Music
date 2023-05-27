de8144cf63903f448d978e335c00e37bb98cc8ec
natanael_daurangga <natanaeldlt06102001@gmail.com>
2023-05-27 11:47:07 +0700

    finalisasi landing page

--- src/assets/components/CourseCard.jsx
+++ src/assets/components/CourseCard.jsx
@@ -5,8 +5,8 @@ import { rupiah } from "../../utility/formatIDR";
 const CourseCard = ({ image, kategori, judul, harga }) => {
   return (
     <Link to={"/"}>
-      <div className="m-auto flex flex-col text-left gap-3 w-[360px] sm:w-[240px] lg:w-[360px] sm:min-h-[240px] min-h-[360px] lg:min-h-[350px] rounded-lg mb-10">
-        <img src={image} alt="" className="h-40 lg:h-60 w-full rounded-lg" />
+      <div className="m-auto flex flex-col text-left gap-3 w-[360px] sm:w-[240px] lg:w-[360px] sm:min-h-[360px] min-h-[360px] lg:min-h-[350px] rounded-lg mb-10">
+        <img src={image} alt="" className="h-60 lg:h-60 w-full rounded-lg" />
         <div className="w-full flex flex-col justify-center gap-2 p-3">
           <span className="text-gray-500">{kategori}</span>
           <span className="text-xl font-bold">{judul}</span>
