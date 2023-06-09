5ac3689b4f886e29ba85c6ec6c3c9869d52e76c3
natanael_daurangga <natanaeldlt06102001@gmail.com>
2023-06-09 17:04:23 +0700

    web config

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,30 +1,63 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Button, Checkbox, Typography } from "@mui/material";
 import Navbar from "../assets/components/Navbar";
 import { ListClass } from "../assets/components/my_class/ListClass";
 import Footer from "../assets/components/Footer";
 import eno from "../assets/img/eno-netral.png";
 import orgMainBiola from "../assets/img/orang-main-biola.png";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+const CheckoutList = ({ isCheck, img, category, name, schedule }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: "2rem",
+        width: "100%",
+        maxWidth: "1920px",
+        borderBottom: "1px solid #BDBDBD",
+        paddingY: "1rem",
+        paddingX: "1rem",
+      }}
+    >
+      <Box sx={{ display: "flex" }}>
+        <Checkbox checked={isCheck} />
+        <ListClass {...{ img, category, name, schedule }} />
+      </Box>
+      <Button
+        sx={{ color: "red" }}
+        variant="text"
+        startIcon={<DeleteForeverIcon color="red" />}
+      >
+        Delete
+      </Button>
+    </Box>
+  );
+};
 
 export const Checkout = () => {
   const dummyData = [
     {
+      isCheck: false,
       img: eno,
       category: "Drum",
       name: "Kursus Drummer Special Coach (Eno Netral)",
       schedule: "Senin, 25 Juli 2022",
     },
     {
+      isCheck: false,
       img: orgMainBiola,
       category: "Biola",
       name: "Biola Mid-Level Course",
       schedule: "Sabtu, 23 Juli 2022",
     },
   ];
-  // TODO: Lanjut benerin checkout sama payment method
+
   return (
     <>
-      <Navbar />
+      <Navbar isLoggedIn={true} />
       <Box
         sx={{
           display: "flex",
@@ -38,18 +71,33 @@ export const Checkout = () => {
           marginX: "auto",
         }}
       >
-        {dummyData.map((data, i) => (
-          <Box
-            sx={{
-              width: "100%",
-              borderBottom: "1px solid #BDBDBD",
-              paddingY: "1rem",
-            }}
-            key={i}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "1rem",
+            width: "100%",
+            maxWidth: "1920px",
+            borderBottom: "1px solid #BDBDBD",
+            paddingY: "1rem",
+            paddingX: "1rem",
+          }}
+        >
+          <Checkbox />
+          <Typography
+            fontSize={"1.25rem"}
+            lineHeight={"1.875rem"}
+            fontWeight={400}
           >
-            <ListClass {...data} />
-          </Box>
+            Pilih Semua
+          </Typography>
+        </Box>
+
+        {/* List course */}
+        {dummyData.map((data, i) => (
+          <CheckoutList {...data} key={i} />
         ))}
+        {/* End List course */}
       </Box>
       <Footer />
     </>
