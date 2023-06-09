1f91dbfe82c65d76a0d6d12cf48e6c21220bd18c
Ardianto50 <antoardi081@gmail.com>
2023-06-09 13:18:15 +0700

    Commit

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,38 @@
+import {
+  Checkbox,
+  Divider,
+  Grid,
+  List,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import NavbarAfter from "../assets/components/after_login/NavbarAfter";
-import { Grid } from "@mui/material";
+import { CheckoutList } from "../assets/components/checkout/CheckoutList";
 
 export const Checkout = () => {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
     <>
       <NavbarAfter />
+      <Grid container md={12} justifyContent={"center"} height={"45rem"}>
+        <Grid item container marginY={5} md={10}>
+          <List sx={{ width: "100%", maxHeight: "20%" }}>
+            <Stack direction="row" alignItems="center">
+              <Checkbox {...label} defaultChecked />
+              <Typography marginX={4} variant="h5">
+                Pilih Semua
+              </Typography>
+            </Stack>
+            <Divider sx={{ marginY: 2 }} />
+          </List>
+          <List sx={{ width: "100%" }}>
+            <CheckoutList />
+            <CheckoutList />
+            <CheckoutList />
+          </List>
+        </Grid>
+      </Grid>
       <Grid
         item
         container
